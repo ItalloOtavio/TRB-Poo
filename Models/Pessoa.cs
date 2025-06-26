1a100af15d064b3b5e484dbc06fcc15112d51a28
ItalloOtavio <otavio.itallo@gmail.com>
2025-06-25 22:48:42 -0300

    Class Pessoa

--- Models/Pessoa.cs
+++ Models/Pessoa.cs
@@ -1,4 +1,13 @@
 class Pessoa
 {
-    
+    public string nome;
+    public string email;
+    public int idade;
+
+    public Pessoa(string nomeDigitado, string emailDigitado, int idadeDigitada)
+    {
+        nome = nomeDigitado;
+        email = emailDigitado;
+        idade = idadeDigitada;
+    }
 }
