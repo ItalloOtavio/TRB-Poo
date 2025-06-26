b952cac4a21095020c7784c60f1f9233938e2c67
Maria Luiza <142853263+Maria5545@users.noreply.github.com>
2025-06-25 22:56:20 -0300

    Updating na classe Pessoa.cs

--- Models/Pessoa.cs
+++ Models/Pessoa.cs
@@ -1,11 +1,9 @@
-class Pessoa
-{
+class Pessoa {
     public string nome;
     public string email;
-    public int idade;
+    public int idade; 
 
-    public Pessoa(string nomeDigitado, string emailDigitado, int idadeDigitada)
-    {
+    public Pessoa (string nomeDigitado, string emailDigitado, int idadeDigitada){
         nome = nomeDigitado;
         email = emailDigitado;
         idade = idadeDigitada;
