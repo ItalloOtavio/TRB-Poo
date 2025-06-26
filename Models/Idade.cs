881848c6b3722f99e80b0de02835e72e4c740ab2
ItalloOtavio <otavio.itallo@gmail.com>
2025-06-25 22:19:04 -0300

    Class Idade

---
+++ Models/Idade.cs
@@ -0,0 +1,11 @@
+class Idade
+{
+    public int MenorIdade;
+    public int MaiorIdade;
+
+    public Idade(int menorIdade, int maiorIdade)
+    {
+        MenorIdade = menorIdade;
+        MaiorIdade = maiorIdade;
+    }
+}
